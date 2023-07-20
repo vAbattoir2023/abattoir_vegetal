2518c467f4cc6479972917cc1f9b2e8fe6d15d0e
moussaillon <moussaillon@mac-design-3.home>
2023-07-20 10:26:20 +0200

    test to cyril

--- public/assets/js/createGroup.js
+++ public/assets/js/createGroup.js
@@ -1,190 +1,97 @@
-document.addEventListener('DOMContentLoaded', (e) => {
+// function getCheck(){
+//   document.addEventListener('DOMContentLoaded', (e) => {
 
-    const myCheckboxes = document.querySelectorAll(".filter-checkbox");
-    const filters = [];
+//     const myCheckboxes = document.querySelectorAll(".filter-checkbox");
+//     const filters = [];
 
-    myCheckboxes.forEach(checkBox => {
-      filters[checkBox.id] = checkBox.checked;
+//     myCheckboxes.forEach(checkBox => {
+//       filters[checkBox.id] = checkBox.checked;
     
-        checkBox.addEventListener('click', (e) => {
-            filters[e.currentTarget.id] = e.target.checked;
+//         checkBox.addEventListener('click', (e) => {
+//             filters[e.currentTarget.id] = e.target.checked;
 
-            console.log(filters);
-            // fonction de raffraichissement de la liste des suggestions
-            refreshUsersSuggestion();
-        });
+//             // console.log('FILTER',filters);
+//             // fonction de raffraichissement de la liste des suggestions
+//             refreshUsersSuggestion();
+//         }); 
+//     });
 
-        
-    });
+//     if (Object.keys(filters).length > 0) {
+//       refreshUsersSuggestion();
+//     }
+//     return filters
+// });
+// }
+// console.log('CHECK',getCheck());
 
-    if (Object.keys(filters).length > 0) {
-      refreshUsersSuggestion();
-    }
-});
-
-
-function refreshUsersSuggestion(){
-  console.log('Raffraichissement de la liste des suggestions');
-  //todo inner html
-}
-
-
-//let inputBox = document.querySelectorAll("input")
-//console.log(inputBox);
-
-
-
-
-
-// let result =[];
-
-// function checkedUserSelection() {
-
-
-//   let myCheckboxes = document.getElementsByClassName("filter-checkbox");
-
-//   console.log(myCheckboxes);
-
-  
-
-//   // for (var i = 0; i < mycheckbox.length; i++) {
-//   //     if (mycheckbox[i].checked) {
-//   //         console.log("check");
-//   //         result.push(mycheckbox[i])
-//   //         break;
-//   //     } else {
-//   //         console.log("pas check");
-//   //         result.splice(mycheckbox[i] ,1)
-//   //     }
-//   // }
-//   // console.log(result);
-  
-//   return result 
-  
+// function refreshUsersSuggestion(){
+//   console.log('Raffraichissement de la liste des suggestions');
+//   //todo inner html
 // }
 
 
-
-
-
-/*
-for (let i = 0; i < checkedBox.length; i++) {
-  checkedBox[i].addEventListener('change',checkedUserSelection)
+// //Définition des utilisateurs avec leurs centres d'intérêt
+//     const array = [
+//         { user:'thomas', center:['foot','guitare','jardiner']},
+//         { user:'jimy', center:['cuisiner','jardiner','conduire']},
+//         { user:'Mona', center:['baseball','conduire','manger']},
+//         { user:'Xavier', center:['foot','manger','jardiner']},
+//     ];
   
-}
-*/
-
-
-//let users =document.getElementById('users')
-/*
-//console.log(array);
-function filterUsers(arrayCenterOfInterest) {
-    for (let i = 0; i < arrayCenterOfInterest.length; i++) {
-        let user= arrayCenterOfInterest[i];
-        let center =user.center
-        if (user) {
-            
-        }
-        console.log(center);
-        //console.log(user.center=='foot');
-        /*
-        users.innerHTML+=`
-                <p>les centre dinterert sont${element.center}</p>
-        `
-        */
-  //  }
-    //let result = arrayCenterOfInterest.filter()
+//    //Fonction pour comparer les centres d'intérêt de deux utilisateurs
+//   /**
+//    *    fonction qui va comparer si lutilisateur 1 a le meme centre 
+//    *    dinterret  enssemble  
+//    * 
+//    * @param {*} utilisateur1 prendra en parametre un premier user
+//    * @param {*} utilisateur2 prendra en parametre un second user
+//    * @returns renvoie les centre dinteret en commun en tableau
+//    */
+  
+//   function comparerCentresInteret(utilisateur1, utilisateur2) {
     
-//}
+//     //la variables qui prendra en compte les centre dinteret de lutilisateur 1
+//     // et les comparrera via filter sur lutilisateur 2 en verifiant via include 
+//     // si linteret de la fonction flecher
+//     let centresInteretCommuns = utilisateur1.center.filter((interet) => utilisateur2.center.includes(interet));
+//     //console.log(centresInteretCommuns);
+//     return centresInteretCommuns;
 
-//filterUsers(array);
-
-
-/*
-    const filter = array.filter((data)=>{
-        return data.center === 'foot' || data.center === 'baseball'
-    })
-console.log(filter.map((data)=>{return data.center}));
-*/
-
-
-/*
-let interet = [];
-
-window.fetch(``)
-        .then(function(httpResponse){
-            //console.log(httpResponse);
-            //console.log(httpResponse);
-            return httpResponse.json();
-            
-            
-        }).then(function(results){
-            //console.log(results);
-            interet = results;
-            
-
-            for (let i = 0; i < inputBox.length; i++) {
-                inputBox[i].addEventListener('change',checkedUserSelection)
-                
-            }
-            
-             
-        });
-*/
-
-
-
-
-//Définition des utilisateurs avec leurs centres d'intérêt
-    const array = [
-        { user:'thomas', center:['foot','guitare','jardiner']},
-        { user:'jimy', center:['cuisiner','jardiner','conduire']},
-        { user:'Mona', center:['baseball','conduire','manger']},
-        { user:'Xavier', center:['foot','manger','jardiner']},
-    ];
-  
-   //Fonction pour comparer les centres d'intérêt de deux utilisateurs
-  /**
-   *    fonction qui va comparer si lutilisateur 1 a le meme centre 
-   *    dinterret  enssemble  
-   * 
-   * @param {*} utilisateur1 prendra en parametre un premier user
-   * @param {*} utilisateur2 prendra en parametre un second user
-   * @returns renvoie les centre dinteret en commun en tableau
-   */
-  
-  function comparerCentresInteret(utilisateur1, utilisateur2) {
-    
-    //la variables qui prendra en compte les centre dinteret de lutilisateur 1
-    // et les comparrera via filter sur lutilisateur 2 en verifiant via include 
-    // si linteret de la fonction flecher
-    let centresInteretCommuns = utilisateur1.center.filter((interet) => utilisateur2.center.includes(interet));
-    console.log(centresInteretCommuns);
-    return centresInteretCommuns;
-
-  }
+//   }
   
   
-  // Comparaison des centres d'intérêt de tous les utilisateurs
-  const resultats = {};
+//   // Comparaison des centres d'intérêt de tous les utilisateurs
+//   const resultats = {};
   
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      const utilisateur1 = array[i];
-      const utilisateur2 = array[j];
-      const centresInteretCommuns = comparerCentresInteret(utilisateur1, utilisateur2);
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = i + 1; j < array.length; j++) {
+//       const utilisateur1 = array[i];
+//       const utilisateur2 = array[j];
+//       const centresInteretCommuns = comparerCentresInteret(utilisateur1, utilisateur2);
   
-      if (centresInteretCommuns.length > 0) {
-        if (!resultats[utilisateur1.user]) {
-          resultats[utilisateur1.user] = [];
-        }
-        resultats[utilisateur1.user].push(utilisateur2.user);
-      }
-    }
-  }
+//       if (centresInteretCommuns.length > 0) {
+//         if (!resultats[utilisateur1.user]) {
+//           resultats[utilisateur1.user] = [];
+//         }
+//         resultats[utilisateur1.user].push(utilisateur2.user);
+//       }
+//     }
+//   }
   
   
   // // Affichage des résultats
-   console.log(resultats);
-  
+   //console.log(resultats);
 
+  const usersBox = document.querySelector('.users');
+
+    function getData(){
+     fetch('http://127.0.0.1:8000/group/getAllUsers')
+      .then(response=>response.json())
+      .then(data=>{ 
+        return data
+      })
+    }
+
+// console.log(getData);
+
+// const filter = allUsers.filter((data)=> data.username == 'redzebra463');
--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Document\Reservation;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,17 +13,50 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/group')]
 class GroupController extends AbstractController
 {
-    #[Route('/add', name: 'app_group')]
-    public function index(): Response
+    #[Route('/select_group', name: 'app_select_group')]
+    public function select_group(UserRepository $userRepository): Response
     {
         // data checkbox
+        $dataCheckbox = [
+            'Animaux',
+            'Environnement',
+            'Art et culture',
+            'Sport',
+            'Voyages',
+            'Musique',
+            'Danse',
+            'Sciences',
+            'Bien-être',
+            'Food',
+            'Activités sociales',
+            'Jeux vidéos',
+        ];
         // centerOfInterest from user
         // all useres from bdd
+        $allUser = $userRepository->findAllFromBdd();
+
+        // echo '<pre>';
+        // var_dump($allUser);
+        // echo '</pre>';
 
         return $this->render('Group/index.html.twig',[
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ReservationController.php',
+            'dataFormCheckbox' => $dataCheckbox,
+            'allUsers' => $allUser
         ]);
     }
+    #[Route('/getAllUsers', name: 'getAllUsers_app')]
+    public function getAllUsers(UserRepository $userRepository): Response
+    {
+        $allUser = $userRepository->findAllFromBdd();
+
+        // echo '<pre>';
+        // var_dump($allUser);
+        // echo '</pre>';
+
+        return $this->json([
+            'allUsers' => $allUser
+        ]);
+    }
+
    
 }
