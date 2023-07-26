283e5de4160231a01f7ae4ba73478c665473ecd3
86Wilfried <wilfried.vig@gmail.com>
2023-07-26 11:42:13 +0200

    26/07

---
+++ symfony/var/cache/dev/url_matching_routes.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file has been auto-generated
+ * by the Symfony Routing Component.
+ */
+
+return [
+    false, // $matchHost
+    [ // $staticRoutes
+        '/home' => [[['_route' => 'app_homeapp_home', '_controller' => 'App\\Controller\\HomeController::index'], null, null, null, true, false, null]],
+        '/register' => [[['_route' => 'app_register', '_controller' => 'App\\Controller\\RegisterController::index'], null, null, null, false, false, null]],
+        '/reservation' => [[['_route' => 'app_reservation', '_controller' => 'App\\Controller\\ReservationController::index'], null, null, null, false, false, null]],
+        '/user' => [[['_route' => 'app_user', '_controller' => 'App\\Controller\\UserController::index'], null, null, null, false, false, null]],
+    ],
+    [ // $regexpList
+        0 => '{^(?'
+                .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+            .')/?$}sDu',
+    ],
+    [ // $dynamicRoutes
+        35 => [
+            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+            [null, null, null, null, false, false, 0],
+        ],
+    ],
+    null, // $checkCondition
+];
