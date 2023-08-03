af607b5ad3396e85af10138a312cf93cbc799957
rwine <132657856+rwine-T@users.noreply.github.com>
2023-08-03 13:52:52 +0200

    up

--- public/assets/js/resa.js
+++ public/assets/js/resa.js
@@ -1,11 +1,2 @@
-/*
 
-function toggleDiv(ticket) {
-    let div = ticket.nextElementSibling;
-    if (div.style.display === 'none') {
-        div.style.display = 'block';
-    } else {
-        div.style.display = 'none';
-    }
-}
-*/
+  
